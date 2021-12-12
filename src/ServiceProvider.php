f3db22406ae311f35c6d92aeeff7650a7fdbc730
Yannoff <yannoff.it@gmail.com>
2021-12-12 17:37:53 +0100

    [ServiceProvider] Fix base class import

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,14 +3,14 @@
 namespace Yannoff\Lumiere\UI;
 
 use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Yannoff\Lumiere\UI\View\Components\Header;
 use Yannoff\Lumiere\UI\View\Components\Md;
 
 /**
  * Register the Lumiere UI components
  */
-class ServiceProvider extends ServiceProvider
+class ServiceProvider extends BaseServiceProvider
 {
     /**
      * Map the components aliases to their respective classname
